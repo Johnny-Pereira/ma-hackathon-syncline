59ac76a91465d0b7203b9c2aaed2424456715e7f
zac-macintyre-1 <zachery.macintyre@moodys.com>
2024-03-17 10:45:30 -0400

    fixed template for resource allocation.  NO CODING

--- Team14/syncline-backend/src/lambda-handler.py
+++ Team14/syncline-backend/src/lambda-handler.py
@@ -1,6 +1,9 @@
+
 def lambda_handler(event, context):
     return {
     "statusCode": 200,
-    "headers": { "Content-Type": "application/json" },
-    "body": "\"hello world\""
+    "headers": { "Content-Type": "application/json",
+                 'Access-Control-Allow-Origin': '*' },
+    'isBase64Encoded': False,
+    "body": "{\"message\": \"hello world\"}"
 }
