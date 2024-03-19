7d0e42aeb2b132bd907ae3aab85175503461f037
Devin Haughey (Moody's) <haugheyd@moodys.com>
2024-03-19 15:56:27 -0400

    adds validation

--- Team13/server/index.js
+++ Team13/server/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
+const express = require('express');
+const validationHandler = require('./validation/validation');
+const app = express();
+const port = 3000;
 
-const app = express()
-app.use(express.json())
+app.get('/', async (req, res) => {
+    try {
+        await validationHandler(req, res);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error');
+    }
+});
 
-app.use('/', require('./routes/queries'))
-
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
