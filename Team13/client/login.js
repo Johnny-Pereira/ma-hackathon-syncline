07db6dce20f2fbca161d56007b81b32ce56bfa45
Prior <michelle.prior@moodys.com>
2024-03-19 11:36:21 -0400

    added comment

--- Team13/client/login.js
+++ Team13/client/login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+
+//Login button - you can use this button component and it will redirect to the login page 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
