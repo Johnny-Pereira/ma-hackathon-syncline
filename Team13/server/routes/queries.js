37d5d2f01ae78878ab47e79265210d12f20e21d3
Alison <141679970+alee300@users.noreply.github.com>
2024-03-19 16:11:08 -0400

    avg rating update

--- Team13/server/routes/queries.js
+++ Team13/server/routes/queries.js
@@ -32,10 +32,20 @@ router.get('/reviews/:OU', (req, res) => {
 
 router.post('/reviews', (req, res) => {
     console.log(req.body)
+    // insert new feedback into feedback table
     pool.query('INSERT INTO blind.feedback ("OUID", "userID", title, feedback, rating, date) VALUES ($1, $2, $3, $4, $5, $6)', [req.body.ouid, req.body.userid, req.body.title, req.body.feedback, req.body.rating, moment().format("YYYY-MM-DD HH:mm:ss")], (error) => {
         if (error) {
             throw error
         }
+
+        // gets updated average rating of OU and updates OU table with new average
+        const currentRating = pool.query('SELECT AVG(rating) FROM blind.feedback WHERE "OUID" = $1', [req.body.ouid], (error, results) => {
+            if(error) { throw error }
+            console.log(`Current OU Rating: ${results.rows[0].avg}`)
+            pool.query('UPDATE blind.ou SET ou_rating = $1 WHERE "code" = $2', [results.rows[0].avg, req.body.ouid], (error, results) => {
+                if(error) { throw error }
+            })
+        })
         res.status(201).send("Review added")
     })
 })
