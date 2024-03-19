005b34feba9922795da4b976c5019a1a0df94868
steven arce <stevenmatthew.arce@gmail.com>
2024-03-19 12:49:55 -0700

    created add review form

--- Team13/client/src/routes/feed/$ouCode.tsx
+++ Team13/client/src/routes/feed/$ouCode.tsx
@@ -1,5 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import AddReviewForm from "@/components/add-review-form";
+
 export const Route = createFileRoute("/feed/$ouCode")({
   component: FeedRoute,
 });
@@ -9,6 +19,17 @@ export function FeedRoute() {
   return (
     <div>
       <h1>OU Code ${ouCode}</h1>
+      <Dialog>
+        <Button asChild>
+          <DialogTrigger>Add a review</DialogTrigger>
+        </Button>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Add a review</DialogTitle>
+          </DialogHeader>
+          <AddReviewForm />
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
