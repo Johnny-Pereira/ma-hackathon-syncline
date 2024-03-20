ee900fd37b62e0bff218fb12d9d96e95c99e1ac5
angelinaebreo <angelina.ebreo@moodys.com>
2024-03-20 11:30:08 -0400

    resolve merge conflict

--- Team13/client/src/routes/index.lazy.tsx
+++ Team13/client/src/routes/index.lazy.tsx
@@ -9,96 +9,24 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-
+} from "@/components/ui/card"
 import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
 
-const sampleReviewData = [
-  {
-    OUname: "sample OU name",
-    date: "3/5/23",
-    teamName: "sample team or project name",
-    review:
-      "sample review text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ratione ipsam mollitia? Natus eaque earum reiciendis maiores illo explicabo iste obcaecati nisi ex, quos voluptatum corporis facere ullam modi quo.",
-    rating: "3.5",
-  },
-  {
-    OUname: "another sample OU name",
-    date: "1/2/23",
-    teamName: "another sample team or project name",
-    review:
-      "another sample review text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ratione ipsam mollitia? Natus eaque earum reiciendis maiores illo explicabo iste obcaecati nisi ex, quos voluptatum corporis facere ullam modi quo.",
-    rating: "5",
-  },
-];
-const sampleReviewData = [{ 'OUname': "Banking OU", 'date': '3/5/23', 'teamName': 'Banking team', 'review': 'sample review text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ratione ipsam mollitia? Natus eaque earum reiciendis maiores illo explicabo iste obcaecati nisi ex, quos voluptatum corporis facere ullam modi quo.', 'rating': '3.5' }, { 'OUname': "CRE OU", 'date': '1/2/23', 'teamName': 'CRE team', 'review': 'another sample review text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ratione ipsam mollitia? Natus eaque earum reiciendis maiores illo explicabo iste obcaecati nisi ex, quos voluptatum corporis facere ullam modi quo.', 'rating': '5' }]
 
+const sampleReviewData = [{ 'OUname': "Banking OU", 'date': '3/5/23', 'teamName': 'Banking team', 'review': 'sample review text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ratione ipsam mollitia? Natus eaque earum reiciendis maiores illo explicabo iste obcaecati nisi ex, quos voluptatum corporis facere ullam modi quo.', 'rating': '3.5' }, { 'OUname': "CRE OU", 'date': '1/2/23', 'teamName': 'CRE team', 'review': 'another sample review text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ratione ipsam mollitia? Natus eaque earum reiciendis maiores illo explicabo iste obcaecati nisi ex, quos voluptatum corporis facere ullam modi quo.', 'rating': '5' }]
 
 function Index() {
   const { loginWithRedirect } = useAuth0();
   return (
-
-    <div className="p-2">
-      <p>
-        Moody's has implemented an anonymous application that allows staff to
-        review team communication/culture and provide recommendations on how
-        teams can grow and become more effective without feeling tied to a
-        particular idea.{" "}
-      </p>
-      <p>
-        This not only fosters a culture of open communication but also enables
-        cross-pollination of ideas between teams and OUs.
-      </p>
-
-      <p>
-        By embracing anonymity, administration can create active change based on
-        feedback and recommendations from their teammates, leading to a more
-        dynamic and effective organization.
-      </p>
-
-      <div
-        className="flex flex-row"
-        style={{ width: "75%", marginLeft: "auto", marginRight: "auto" }}
-      >
-        {sampleReviewData.map((review) => (
-          <div className="basis-1/2" style={{ padding: "10px" }}>
-            {" "}
-            <Card style={{ minHeight: "300px" }}>
-              <CardHeader>
-                <CardTitle>
-                  <pre>
-                    {review.OUname} {"\n"}
-                    {review.teamName}
-                  </pre>
-                </CardTitle>
-                <CardDescription>
-                  <div>{review.date}</div>
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-row">
-                  <div style={{ padding: "10px" }}>
-                    <ThumbDownIcon /> <ThumbUpIcon />
-                  </div>
-                  <p>{review.review}</p>
-                </div>
-              </CardContent>
-              <CardFooter>
-                <p>{review.rating}</p>
-              </CardFooter>
-            </Card>
-          </div>
-        ))}
-      </div>
-    </div>
     <>
 
-      <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
+      <div style={{ width: "%", marginLeft: "auto", marginRight: "auto" }}>
         <p>Moody's has implemented an anonymous application that allows staff to review team communication/culture and provide recommendations on how teams can grow and become more effective without feeling tied to a particular idea. </p>
         <p>
           This not only fosters a culture of open communication but also enables cross-pollination of ideas between teams and OUs.
